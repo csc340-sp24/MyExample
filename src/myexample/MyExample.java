eafb678dac9323d914e726e1f79b28c49fb09565
csc340-f23 <csc340f23@gmail.com>
2024-01-10 12:29:27 -0500

    modified file

--- src/myexample/MyExample.java
+++ src/myexample/MyExample.java
@@ -12,6 +12,7 @@ public class MyExample {
      */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Added  a new line");
     }
     
 }
