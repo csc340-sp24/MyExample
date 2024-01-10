eb4317d451abd83870f6fc6e41338c1cc243c3fc
csc340-f23 <csc340f23@gmail.com>
2024-01-10 12:25:05 -0500

    initial commit

---
+++ src/myexample/MyExample.java
@@ -0,0 +1,17 @@
+
+package myexample;
+
+/**
+ *
+ * @author sunny
+ */
+public class MyExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
